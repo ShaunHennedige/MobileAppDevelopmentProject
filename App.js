19df2cd41370014f17cfd97042f68c3180fb0b6a
Shaun Hennedige <99944769+ShaunHennedige@users.noreply.github.com>
2023-05-18 11:14:49 +0530

    main1

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {StatusBar} from 'expo-status-bar';
+import {Provider as PaperProvider, MD3DarkTheme} from 'react-native-paper';
+import Main from './src/Main';
+import {AuthProvider} from './src/util/AuthContext';
+
+const theme = {
+  ...MD3DarkTheme,
+  mode: 'adaptive',
+  roundness: 2,
+  colors: {
+    ...MD3DarkTheme.colors,
+  },
+};
+
+export default function App() {
+  return (
+    <PaperProvider theme={theme}>
+      <AuthProvider>
+        <StatusBar style="auto" />
+        <Main />
+      </AuthProvider>
+    </PaperProvider>
+  );
+}
---
+++ app.config.js
@@ -0,0 +1,52 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'RouteSync',
+    slug: 'computing-project-app',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'dark',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    plugins: [
+      [
+        'expo-barcode-scanner',
+        {
+          cameraPermission: 'Allow $(PRODUCT_NAME) to access camera.',
+        },
+      ],
+
+    ],
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API,
+        },
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    extra: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+    },
+  },
+};
